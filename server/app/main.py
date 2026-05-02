1f4b59d2ec8aab8f19107a4093ca0d385293869b
Areeb455 <areebimam466@gmail.com>
2026-05-03 01:24:22 +0530

    cors

--- server/app/main.py
+++ server/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Medule API", version="2.2.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )

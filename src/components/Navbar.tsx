df74378aff4e639bca078016a9822480fecf9aee
Areeb455 <141515705+Areeb455@users.noreply.github.com>
2026-05-13 18:05:32 +0530

    Fix email formatting in ALLOWED_EMAILS array

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 
 const ALLOWED_EMAILS = [
   "yusufusmani910@gmail.com",
-  "areebimam466@gmail.com"
+  "areebimam466@gmail.com",
   "omsh0401@gmail.com",
   "areebimam455@gmail.com"
 ];
